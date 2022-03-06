46455baa55b37e17378e065fd463154e3587f396
romcohen <romcohen@cs.huji.ac.il>
2022-03-07 00:52:11 +0200

    Added documentation

--- FullBody.py
+++ FullBody.py
@@ -6,85 +6,126 @@ import datetime
 
 
 class Body:
+    """
+    Body class is responsible to validate the points created from the cv network, detect a repetition, save the data,
+    Run the logics of the system
+    """
 
     def __init__(self, run_dir, training_dir=None):
-        self.run_dir = run_dir
-        self.__standing_line_points = None
-        self.__user_on_standing_line = False
+        self.run_dir = run_dir  # Directory to save the ouput files
+        self.__standing_line_points = None  # Coordinates of the standing line
+        self.__user_on_standing_line = False  # Flag to indicate if the user is currently on the standing line
         self.__frame_sizes = None
-        self.__ready = False
-        self.__valid_points = False
-        self.class_body_points = None
-        self.class_body_points_centered = None
-        self.class_body_points_3d = None
-        self.ignore_indexes = get_ignored_indexes(BODY_PARTS_LIST, BODY_PARTS_LIST_CLASS)
-        self.pixel_delta_from_horizontal = 30
-        self.body_lengths = None
-        self.init_body_lengths = None
-        self.frame_index = None
-        self.__user_height_in_pixel = None
-        self.valid_points_list = list()
-        self.calibrate_mode = True
-        self.__init_pose = None
-        self.__lowest_squat_point = None
-        self.sample_squat_mode = False
-        self.__cur_squat_frames = list()
-        self.__cur_lowest_squat_point = None
-        self.training_dir = training_dir
+        self.__ready = False  # Flag that indicated if class is calibrated to the users measurements
+        self.__valid_points = False  # Flag that indicates that the class got valid points
+        self.class_body_points = None  # The current points passed through the class
+        self.class_body_points_centered = None  # The current points after centering them
+        self.class_body_points_3d = None  # The current points after finding the 3d form
+        self.ignore_indexes = get_ignored_indexes(BODY_PARTS_LIST, BODY_PARTS_LIST_CLASS)  # Body parts to ignore
+        self.pixel_delta_from_horizontal = 30  # the maximum distance allowed between the user and the standing line
+        self.body_lengths = None  # Current lengths between body points
+        self.init_body_lengths = None  # On class init get the users body lengths while standing, mainly for 3d points
+        self.frame_index = None  # Index of the current frame
+        self.__user_height_in_pixel = None  # Height of the user while standing in pixeles from the standing line
+        self.valid_points_list = list()  # List of list of valid points
+        self.calibrate_mode = True  # Flag for calibration mode of the class
+        self.__init_pose = None  # Init the squat capture
+        self.__lowest_squat_point = None  # After refrencing a squat save the minimum height a user reaches in squat
+        self.sample_squat_mode = False  # Reference mode for the squat
+        self.__cur_squat_frames = list()  # The frames captured in the current squat
+        self.__cur_lowest_squat_point = None  # current lowest point of the user
+        self.training_dir = training_dir  # Path to the training dir
 
     def is_ready(self):
+        """
+        :return: If the object is ready, has valid points
+        """
         return self.__ready
 
     def set_standing_line(self, points, frame_sizes):
+        """
+        Set where the standing line is, coordinates
+        :param points: coordinates of the standing line
+        :param frame_sizes: the width and length of the frames
+        """
         self.__standing_line_points = points
         self.__frame_sizes = frame_sizes
 
     def on_standing_line(self):
+        """
+        :return: True if user was detected on the standing line and false otherwise
+        """
         return self.__user_on_standing_line
 
     def got_valid_points(self):
+        """
+        :return: True if the points given to the object are valid, false otherwise
+        """
         return self.__valid_points
 
     def calibrate_class(self, body_points, frame_index):
-        self.frame_index = frame_index
-        if not self.__insert_points(body_points):
+        """
+        Calibrate the class with the users initial body points, when the user is standing the object will calculate
+        their height and distances from each body part. mainly done to convert 2d points into 3d points
+        :param body_points: List of points representing the body parts of the user
+        :param frame_index: Index of the frame
+        :return: The proper instruction if there is an error
+        """
+        self.frame_index = frame_index  # Set frame index
+        if not self.__insert_points(body_points):  # If not all relevant points where detected
             self.__ready = False
             return INSTRUCTIONS['missing_points']
-        if not self.__set_user_on_standing_line():
+        if not self.__set_user_on_standing_line():  # If the user is not standing on the standing line
             self.__ready = False
             return INSTRUCTIONS['standing_line']
-        self.__calculate_user_height()
+        self.__calculate_user_height()  # Set the height of the user
         self.__init_pose = copy.deepcopy(self.class_body_points)
-        self.__get_body_lengths()
-        self.__ready = True
+        self.__get_body_lengths()  # Calculate the body lengths of the user
+        self.__ready = True  # Set the object as calibrated and ready
         return INSTRUCTIONS['calibrated']
 
     def init_squat_ref(self):
+        """
+        Set the distance a user should be going down during squat from a reference squat, this methods inits all
+        properties of the motion. while conditions are not met the initial setting would not be complited
+        :return: True if the squat answered all conditions
+        """
         cur_nose_height = self.__frame_sizes[Y] - self.class_body_points['Nose'][Y]
         init_nose_height = self.__frame_sizes[Y] - self.__init_pose['Nose'][Y]
-        if init_nose_height * 0.75 < cur_nose_height and not self.__lowest_squat_point:
+        if init_nose_height * 0.75 < cur_nose_height and not self.__lowest_squat_point:  # Squat is not low enough
             return False
-        if not self.__lowest_squat_point:
+        if not self.__lowest_squat_point:  # Update lowest point detected
             self.__lowest_squat_point = cur_nose_height
-        if cur_nose_height < self.__lowest_squat_point:
+        if cur_nose_height < self.__lowest_squat_point:  # Update lowest point detected
             self.__lowest_squat_point = cur_nose_height
             return False
-        if abs(cur_nose_height - init_nose_height) < 10:
+        if abs(cur_nose_height - init_nose_height) < 10:  # If the user is standing again after hitting the lowest point
             print('Finished: {}'.format(self.__lowest_squat_point / self.__user_height_in_pixel * 100))
             return True
 
     def check_body_points(self, points, frame_index):
-        self.frame_index = frame_index
+        """
+        Check if given points are valid
+        :param points: Point list to check
+        :param frame_index: frame index
+        :return: True if points given are valid, False otherwise
+        """
+        self.frame_index = frame_index  # Set frame index
         self.__valid_points = False
-        if not self.__insert_points(points):
+        if not self.__insert_points(points):  # If points are missing
             return INSTRUCTIONS['missing_points']
-        if not self.__set_user_on_standing_line():
+        if not self.__set_user_on_standing_line():  # If user is not on standing line
             return INSTRUCTIONS['standing_line']
         self.__valid_points = True
 
     def get_squat(self, squat_idx=-1):
+        """
+        Get a saved squat by index
+        :param squat_idx: index representing the saved squat
+        :return: The squat points set after they where centered and at 3d format
+        """
         if len(self.valid_points_list) == 0:
-            return  False
+            return False
         elif squat_idx != -1 and squat_idx >= len(self.valid_points_list):
             return False
         else:
@@ -100,6 +141,10 @@ class Body:
             return centered, three_d
 
     def squat(self):
+        """
+        Capture a squat motion
+        :return: True if the squat was completed, False otherwise
+        """
         cur_height = self.__frame_sizes[Y] - self.class_body_points['Nose'][Y]  # Current user height
         if len(self.__cur_squat_frames) == 0:  # The beginning of the squat
             if self.__user_height_in_pixel - 5 < cur_height:  # If the user is standing - beginning of movement
@@ -123,6 +168,11 @@ class Body:
         return False
 
     def __insert_points(self, body_points):
+        """
+        Insert points to the object, checks if all relevant points where given
+        :param body_points: Point list
+        :return: True if points where inserted False otherwise
+        """
         self.class_body_points = dict()
         for i, point in enumerate(body_points):
             if i in self.ignore_indexes:
@@ -167,6 +217,10 @@ class Body:
             self.class_body_points["R{}".format(body_part)] = (right[X], int(math.ceil(y_avg)))
 
     def __set_user_on_standing_line(self):
+        """
+        Check if user is on standing line and set the current status to the object
+        :return:
+        """
         if not self.class_body_points or not self.__standing_line_points:
             return
         left_heel = self.class_body_points['LHeel']
@@ -184,6 +238,10 @@ class Body:
         return True
 
     def __calculate_user_height(self):
+        """
+        Calculate the users height in pixeles
+        :return:
+        """
         if not self.class_body_points or not self.__standing_line_points:
             return
         nose_y = self.class_body_points['Nose'][Y]
@@ -191,6 +249,10 @@ class Body:
         self.__user_height_in_pixel = height
 
     def __get_body_lengths(self):
+        """
+        Calculate the length between body parts and save to object as dictionary
+        :return:
+        """
         self.init_body_lengths = dict()
         self.init_body_lengths['r_heel_ankle'] = get_2d_distance((self.__init_pose['RAnkle'][X],
                                                                   self.__init_pose['RHeel'][Y]),
@@ -212,6 +274,10 @@ class Body:
         self.init_body_lengths['shoulder_nose'] = get_2d_distance(mid_shoulder, self.__init_pose['Nose'])
 
     def get_network_point_format(self):
+        """
+        Get the points format the cv uses
+        :return: points on cv format
+        """
         output = list()
         for i, body_part in enumerate(BODY_PARTS_LIST):
             if i in self.ignore_indexes:
@@ -221,29 +287,38 @@ class Body:
         return output
 
     def get_class_points_list(self):
+        """
+        :return: Returns point list the way the class uses them
+        """
         output = list()
         for body_part in BODY_PARTS_LIST_CLASS:
             output.append(self.class_body_points_centered[body_part])
         return output
 
     def get_class_3d_points_list(self):
+        """
+        :return: 3d points at the class format
+        """
         output = list()
         for body_part in BODY_PARTS_LIST_CLASS:
             output.append(self.class_body_points_3d[body_part])
         return output
 
     def output_points(self):
-        if not self.run_dir:
+        """
+        Output all points, sets of points captured during the run of the object
+        """
+        if not self.run_dir:  # No run directory
             return False
-        self.__get_body_lengths()
-        for squat_points in self.valid_points_list:
+        self.__get_body_lengths()  # Get the body lengths of the user
+        for squat_points in self.valid_points_list:  # For each squat sampled
             three_d, centered = list(), list()
             start_frame, end_frame = squat_points[0]['frame_idx'], squat_points[-1]['frame_idx']
             for points in squat_points:
                 self.class_body_points = copy.deepcopy(points)
-                self.center_r_heel()
-                self.rotate_points_around_r_heel()
-                self.create_3d_points()
+                self.center_r_heel()  # Center the points
+                self.rotate_points_around_r_heel()  # Rotate the points around the right ancle
+                self.create_3d_points()  # Create a 3d version of the points
                 centered.append(self.get_class_points_list())
                 three_d.append(self.get_class_3d_points_list())
 
@@ -259,6 +334,9 @@ class Body:
                                 'three_d_points_id_{}-{}.txt'.format(start_frame, end_frame), 'three_d_points')
 
     def output_data_set_points(self):
+        """
+        Output points as a data set
+        """
         self.__get_body_lengths()
         for squat_points in self.valid_points_list:
             three_d = list()
@@ -285,6 +363,10 @@ class Body:
         self.valid_points_list = list()
 
     def center_r_heel(self):
+        """
+        Move the right heel to (0, 0)
+        :return:
+        """
         self.class_body_points_centered = dict()
         heel_point = self.class_body_points['RHeel']
         move_x = heel_point[X]
@@ -294,6 +376,10 @@ class Body:
                                                           (self.class_body_points[body_part][Y] - move_y))
 
     def rotate_points_around_r_heel(self):
+        """
+        Rotate the coordinates
+        :return:
+        """
         r_ankle_coord = self.class_body_points_centered['RHeel']
         if r_ankle_coord != (0, 0):
             return
@@ -304,6 +390,9 @@ class Body:
                                                                       math.pi)
 
     def create_3d_points(self):
+        """
+        Hard coded covert each body part point to 3d point
+        """
         self.class_body_points_3d = dict()
         cur = copy.deepcopy(self.class_body_points_centered['RHeel'])
         self.class_body_points_3d['RHeel'] = cur + tuple([0])
--- utils.py
+++ utils.py
@@ -5,6 +5,11 @@ import math
 import os
 import random
 import pickle
+import copy
+
+"""
+Utils file for common functions along the system
+"""
 
 X = 0
 Y = 1
@@ -14,24 +19,26 @@ MULTI_LABELS = ['high_waste', 'knee_collapse', 'lifting_heels']
 BINARY_LABELS = ['bad', 'good']
 
 BODY_PARTS_LIST_CLASS = ['Nose', 'REye_c', 'LEye_c', 'RShoulder', 'LShoulder', 'RHip', 'LHip', 'RKnee', 'LKnee',
-                         'RAnkle', 'LAnkle', 'RHeel', 'LHeel', 'RTows', 'LTows']
+                         'RAnkle', 'LAnkle', 'RHeel', 'LHeel', 'RTows', 'LTows']  # Relevant body parts for the system
 
 BODY_PARTS_LIST = ['Nose', 'REye_l', 'REye_c', 'REye_r', 'LEye_r', 'LEye_c', 'LEye_l', 'RCheek', 'LCheek', 'RLip',
                    'LLip', 'RShoulder', 'LShoulder', 'RElbow', 'LElbow', 'RWrist', 'LWrist', 'RHand_r', 'LHand_l',
                    'RHand_c', 'LHand_c', 'RHand_l', 'LHand_r', 'RHip', 'LHip', 'RKnee', 'LKnee', 'RAnkle', 'LAnkle',
-                   'RHeel', 'LHeel', 'RTows', 'LTows']
+                   'RHeel', 'LHeel', 'RTows', 'LTows']  # All body parts the cv returns
 
 POSE_PAIRS = [(0, 1), (1, 2), (2, 3), (3, 7), (0, 4), (4, 5), (5, 6), (6, 8), (9, 10), (11, 12), (11, 13), (13, 15),
               (15, 17), (15, 19), (15, 21), (17, 19), (12, 14), (14, 16), (16, 18), (16, 20), (16, 22), (18, 20),
               (11, 23), (12, 24), (23, 24), (23, 25), (24, 26), (25, 27), (26, 28), (27, 29), (28, 30), (29, 31),
-              (30, 32), (27, 31), (28, 32)]
+              (30, 32), (27, 31), (28, 32)]  # Pairs or body parts that are connected
 
+# Map a string to index
 BODY_PARTS_MAP_INDEX = {'Nose': 0, 'REye_l': 1, 'REye_c': 2, 'REye_r': 3, 'LEye_r': 4, 'LEye_c': 5, 'LEye_l': 6,
                         'RCheek': 7, 'LCheek': 8, 'RLip': 9, 'LLip': 10, 'RShoulder': 11, 'LShoulder': 12, 'RElbow': 13,
                         'LElbow': 14, 'RWrist': 15, 'LWrist': 16, 'RHand_r': 17, 'LHand_l': 18, 'RHand_c': 19,
                         'LHand_c': 20, 'RHand_l': 21, 'LHand_r': 22, 'RHip': 23, 'LHip': 24, 'RKnee': 25, 'LKnee': 26,
                         'RAnkle': 27, 'LAnkle': 28, 'RHeel': 29, 'LHeel': 30, 'RTows': 31, 'LTows': 32}
 
+# Key to instruction for the UI - Not all instructions are implemented
 INSTRUCTIONS = {
     'keep_still': "Keep still",
     'heels_not_horizontal': "Make sure your are faced to the camera standing straight\nand the camera is positioned "
@@ -42,17 +49,24 @@ INSTRUCTIONS = {
     'invalid_points': 'Points are not right',
     'standing_line': 'Please stand on the standing line',
     'missing_points': 'Missing some needed points',
-    'raising_heels': 'Keep your feet flat on the floor',
+    'lifting_heels': 'Keep your feet flat on the floor',
     'high_waste': 'Bend forward less',
     'knee_collapse': 'Keep knees aligned with your feet',
     'but_forward': 'Imagine sitting down on a chair',
     'head_position': 'Look straight ahead',
     'shoulder_stress': 'Release your shoulders',
-    'round_back': 'Engage your core'
+    'round_back': 'Engage your core',
+    'partial_motion': 'Try to go down more'
 }
 
 
 def get_ignored_indexes(full_list, class_list):
+    """
+    Return the indexes of body parts the system should ignore
+    :param full_list:
+    :param class_list:
+    :return:
+    """
     output = list()
     for idx, part in enumerate(full_list):
         if part not in class_list:
@@ -61,6 +75,15 @@ def get_ignored_indexes(full_list, class_list):
 
 
 def plot_points(point_list, label_list, title, out_dir, file_name, file_dir):
+    """
+    Plots points given on a graph and outputs it into the path given in params
+    :param point_list: List of points to plot
+    :param label_list: Labels representing the name of the body parts
+    :param title: Title of the plot
+    :param out_dir: Output directory of the files
+    :param file_name: Name of the file to save
+    :param file_dir: Directory to save the file inside of the out_dir
+    """
     x = list()
     y = list()
     labels = list()
@@ -76,8 +99,10 @@ def plot_points(point_list, label_list, title, out_dir, file_name, file_dir):
             labels.append(label_list[i])
     plt.title(title)
     plt.scatter(np.array(x), np.array(y))
-    plt.xlim((int(min_x) - 100, int(max_x) + 100))
-    plt.ylim((int(min_y) - 100, int(max_y) + 100))
+    # plt.xlim((int(min_x) - 1, int(max_x) + 1))
+    # plt.ylim((int(min_y) - 1, int(max_y) + 1))
+    plt.xlim((-4, 14))
+    plt.ylim((-1, 14))
     for i, txt in enumerate(labels):
         plt.annotate("{}".format(txt), (x[i], y[i]))
     file_path = os.path.join(out_dir, file_dir, file_name)
@@ -87,6 +112,16 @@ def plot_points(point_list, label_list, title, out_dir, file_name, file_dir):
 
 
 def plot_profile_points(point_list, label_list, title, out_dir, file_name, file_dir):
+    """
+    Plot 3d points at the profile of the user
+    :param point_list: List of 3d points
+    :param label_list: List of labels for each point representing its body part name
+    :param title: Title of the plot
+    :param out_dir: Output directory
+    :param file_name: Name of the file to save
+    :param file_dir: Directory to save the file inside the out_dir
+    :return:
+    """
     x = list()
     y = list()
     labels = list()
@@ -115,6 +150,11 @@ def plot_profile_points(point_list, label_list, title, out_dir, file_name, file_
 
 
 def print_points(point_list, body_part_list):
+    """
+    Print a point list in a certain format
+    :param point_list: List of points to print
+    :param body_part_list: List of body part, index to string
+    """
     output = 'Detected: '
     for i in range(len(point_list)):
         output += "{}: {}, ".format(body_part_list[i], point_list[i])
@@ -122,6 +162,11 @@ def print_points(point_list, body_part_list):
 
 
 def create_run_dir():
+    """
+    Create a run directory for a certain run, named with the date and time of the run and inside of the directory there
+    are sub directory for each kind of output. For testing and creating datasets
+    :return: The path to the new directory
+    """
     output_dir = 'run_dir'
     cur_string_time = "".join(str(datetime.datetime.now()).split(" ")[1].split(".")[0].split(":"))
     cur_string_date = "".join(str(datetime.datetime.now()).split(" ")[0].split("-"))
@@ -136,6 +181,12 @@ def create_run_dir():
 
 
 def rotate_point(point, radians):
+    """
+    Rotate a point around the (0,0)
+    :param point: Body point
+    :param radians: Radians to rotate
+    :return: Rotated point
+    """
     x, y = point
     xx = x * math.cos(radians) + y * math.sin(radians)
     yy = -x * math.sin(radians) + y * math.cos(radians)
@@ -143,10 +194,23 @@ def rotate_point(point, radians):
 
 
 def get_2d_distance(point1, point2):
+    """
+    Get the distance between two points
+    :param point1: First point
+    :param point2: Second point
+    :return:
+    """
     return math.sqrt(math.pow(int(point1[X]) - int(point2[X]), 2) + math.pow(int(point1[Y]) - int(point2[Y]), 2))
 
 
 def get_z_coordinate(point, distance, relative_p):
+    """
+    Calculate where the z point will given the old distance of its 2d place from the relative point
+    :param point: Point to get z coordinate
+    :param distance: Initial distance between reference point and given point on the 2d dim
+    :param relative_p: Relative point
+    :return: The z coordinate of the point
+    """
     temp = math.pow(distance, 2) - math.pow((point[X] - relative_p[X]), 2) - math.pow((point[Y] - relative_p[Y]), 2)
     temp = round(temp, 2)
     if temp < 0:
@@ -155,12 +219,26 @@ def get_z_coordinate(point, distance, relative_p):
 
 
 def write_to_txt_points(points, run_dir, file_name, dir_name):
+    """
+    Write points into a text file
+    :param points: A list of points to write into the text
+    :param run_dir: Run directory to save the file
+    :param file_name: Name of the file to be saved
+    :param dir_name: Name of the directory to save the file inside the run_dir
+    """
     file_path = os.path.join(run_dir, dir_name, file_name)
     with open(file_path, 'w') as writer:
         writer.write(str(points))
 
 
 def load_txt_point(run_dir, file_name, dir_name):
+    """
+    Load points from a text file
+    :param run_dir: Path to the run directory
+    :param file_name: Name of the points file
+    :param dir_name: Name of the directory of the points inside the run_dir
+    :return: A list of points
+    """
     file_path = os.path.join(run_dir, dir_name, file_name)
     with open(file_path, 'r') as reader:
         text = reader.read()
@@ -170,6 +248,11 @@ def load_txt_point(run_dir, file_name, dir_name):
 
 
 def load_random_squat(squat_type='good'):
+    """
+    Load a random set of points representing a squat
+    :param squat_type: Type of squat, the kind of classification it got
+    :return: Tuple of lists of points
+    """
     working_dir = 'training_data_set'
     point_types = ['centered_points', 'three_d_points']
     points = list()
@@ -182,6 +265,15 @@ def load_random_squat(squat_type='good'):
 
 
 def get_standing_line(width, height, height_fraction, line_fraction):
+    """
+    Given the width and height of an image return and the standing line specs, height on the image and width of the line
+    The function will return the coordinates the standing line should be presented on
+    :param width: Width of the image
+    :param height: Height of the image
+    :param height_fraction: Fraction of the height of the image where the standing line will be positioned
+    :param line_fraction: Width of the line at the ration of the image width
+    :return: Standing line coordinates
+    """
     middle_frame = width / 2
     height_position = height - (height * height_fraction)
     fraction_width = width * line_fraction
@@ -191,6 +283,13 @@ def get_standing_line(width, height, height_fraction, line_fraction):
 
 
 def get_data_set(directory, folder_name, multi=True):
+    """
+    Get a data set from a folder
+    :param directory: Path to dataset directory
+    :param folder_name: Name of the containing folder
+    :param multi: Should the dataset be multiclass or binary
+    :return: The dataset and labels
+    """
     output_data = list()
     output_labels = list()
     if multi:
@@ -222,6 +321,11 @@ def get_data_set_multi(directory, folder_name):
 
 
 def find_min_len(data_set):
+    """
+    Find the smallest frame count in a dataset
+    :param data_set: dataset object
+    :return: Minimum frame samples in the dataset
+    """
     min_len = 1000
     for video_frames in data_set:
         curr_len = len(video_frames)
@@ -275,5 +379,3 @@ def load_models(model_paths):
         model = pickle.load(loaded_file)
         output_models.append(model)
     return output_models
-
-
