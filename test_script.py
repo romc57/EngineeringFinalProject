de820cea4fcd0469e2d4576248b5c8a63f2db2c8
Roy Schossberge <royschossberge@cs.huji.ac.il>
2022-03-09 21:24:10 +0200

    document + test script.

--- test_script.py
+++ test_script.py
@@ -10,19 +10,7 @@ THREE_DIM = r'three_d_points'
 TWO_DIM = r'centered_points'
 
 if __name__ == '__main__':
-    # data, tags = get_data_set(TRAINING_DATA, THREE_DIM)
-    # print(f'Number of samples in data : {len(tags)}, train set size = {(1 - TEST_SET_SIZE) * len(tags)}')
-    # data = normalize_data_len(data)
-    # data_knn = np.array(data)
-    # train_x, train_y, test_x, test_y = split_data_train_test(data_knn, tags, TEST_SET_SIZE)
-    # dim = train_x.shape[1] * train_x.shape[2] * train_x.shape[3]
-    # # runs knn:
-    # model = SimpleKNN(1, dim)
-    # train_x_knn = train_x.reshape(len(train_x) , -1)
-    # test_x_knn = test_x.reshape(len(test_x) , -1)
-    # evaluate_knn(train_x_knn, train_y, test_x_knn, test_y, model, 'accuracy')
-
-    # runs knn multi:
+    # runs knn:
     data_multi, tags_multi = get_data_set(TRAINING_DATA, TWO_DIM, True)
     print(f'Number of samples in data : {len(tags_multi)}, train set size = {(1 - TEST_SET_SIZE) * len(tags_multi)}')
     data_multi = normalize_data_len(data_multi)
@@ -44,4 +32,5 @@ if __name__ == '__main__':
 
     model_net = LinearNeuralNet(dim)
     train_model(model_net, data_manager, EPOCHS, learning_rate=LR, weight_decay=WD)
-    print(f'NN for {TWO_DIM} return accuracy of {evaluate(model_net, data_test_manager.get_data_iterator(), binary_accuracy)} \nfor the test set.')
+    print(f'NN for {TWO_DIM} return accuracy of '
+          f'{evaluate(model_net, data_test_manager.get_data_iterator(), binary_accuracy)} \nfor the test set.')
