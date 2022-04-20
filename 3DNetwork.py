cf8b1ca98edf0374845ace6fae29c89f5f72bb52
Roy Schossberge <royschossberge@cs.huji.ac.il>
2022-04-20 10:09:23 +0300

    3D file

--- 3DNetwork.py
+++ 3DNetwork.py
@@ -8,7 +8,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
 # For static images:
-IMAGE_FILES = []
+IMAGE_FILES = list()
 BG_COLOR = (192, 192, 192) # gray
 # with mp_pose.Pose(
 #         static_image_mode=True,
