636eec5f0d5d3b64eb297298fa8787b512dbc170
Roy Schossberge <royschossberge@cs.huji.ac.il>
2022-05-10 21:15:21 +0300

    running multiClass classification

--- train_multi_exercise.py
+++ train_multi_exercise.py
@@ -2,11 +2,30 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import main
+import os
+import utils
 
 
-def create_data_set(root_directory: str, classes: dict, target_path: str):
+def results_to_coordinates(results):
+    points = list()
+    if results.pose_landmarks:
+        for id, lm in enumerate(results.pose_world_landmarks.landmark):
+            cx, cy, cz = lm.x, lm.y, lm.z
+            points.append((cx, cy, cz))
+    return points
+
+
+def create_data_set(root_directory: str, folder_name: str, run_directory: str):
     # TODO : run on directory and save frames. normalize and tag. save txt in target
-    pass
+    path = os.path.join(root_directory)
+    for folder in os.listdir(path):
+        root = os.path.join(root_directory, folder)
+        id_video = 0
+        for video in os.listdir(root):
+            points = get_key_points(os.path.join(root, video))
+            utils.write_to_txt_points(points, run_directory, f"{id_video}_{folder}.txt",
+                                      f'{folder} {folder_name}')
+            id_video += 1
 
 
 def normalize_data(data_set: list) -> list:
@@ -49,14 +68,18 @@ def get_key_points(video_path):
             # Flip the image horizontally for a selfie-view display.
             cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
             if cv2.waitKey(5) & 0xFF == 27:
+                output = list()
                 for results in all_results:
-                    mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
-                    print(results.pose_world_landmarks)
+                    # mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+                    point = results_to_coordinates(results)
+                    if point:
+                        output.append(point)
                 break
         cap.release()
+        return output
 
 
 if __name__ == '__main__':
-    video_path = r"D:\projects_files\final project\push ups\pexels-kampus-production-8171374.mp4"
-    get_key_points(video_path)
-
+    video_path = r"D:\projects_files\final project"
+    # get_key_points(video_path)
+    create_data_set(video_path, 'data', r"D:\projects_files")
